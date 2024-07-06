e6acb8170c158d7b5cfa8e892fe53f1e63c39d9d
Ney Perea <nperea75@gmail.com>
2024-07-05 20:12:32 -0500

    se modifica todo el proyecto por una version de prueba del encriptador

--- app.js
+++ app.js
@@ -1,30 +1,48 @@
-let encriptado = document.getElementById("encriptado");
-fraseOriginal=prompt("inserte una frase");
-for (let i = 0; i < fraseOriginal.length; i++) {
-    if (fraseOriginal[i] === "a") {
-        fraseOriginal = fraseOriginal.replace("a", "4");
-    }
-  else if (fraseOriginal[i] === "e") {
-      fraseOriginal = fraseOriginal.replace("e", "3");
-  }
+
+    window.addEventListener("load", function() {
+
+  let textoCapturado = prompt("escriba la frase a encriptar");
+  let capturado = document.getElementById("capturado");
+  let textoEncriptado = document.getElementById("encriptar");
+  let desencripatado = document.getElementById("desencriptar");
+  let textoParaDesencriptar ="";
   
-  else if (fraseOriginal[i] === "i") {
-    fraseOriginal = fraseOriginal.replace("i", "1");
+
+  capturado.innerText = textoCapturado;
+
+//seccion de encriptado
+  const transcript = {
+    e: "enter",
+    i: "imes",
+    a: "ai",
+    o: "ober",
+    u: "ufat",
+  };
+
+  for (let clave in transcript) {
+    textoCapturado = textoCapturado.replaceAll(clave, transcript[clave]);
   }
 
-  else if (fraseOriginal[i] === "o") {
-    fraseOriginal = fraseOriginal.replace("o", "0");
+  textoEncriptado.innerText = textoCapturado;
+  textoParaDesencriptar = textoCapturado;
+  
+  
+
+ //seccion de desencriptado
+  const untranscript = {
+    enter:"e",
+    imes:"i",
+    ai:"a",
+    ober:"o",
+    ufat:"u",
+  };
+
+  for (let clave in untranscript) {
+    textoParaDesencriptar = textoParaDesencriptar.replaceAll(clave, untranscript[clave]);
   }
 
-  else if (fraseOriginal[i] === "u") {
-    fraseOriginal = fraseOriginal.replace("u", "5");
-  }
-    
-  }
+  desencripatado.innerText = textoParaDesencriptar;
 
-encriptado.innerText = fraseOriginal;
 
-console.log(fraseOriginal);
-console.log(encriptado);
-console.log(typeof(encriptado.innertext));
-console.log(encriptado.innerText);
+});
+
